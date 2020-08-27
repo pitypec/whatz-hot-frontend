dba066120b593bfdad007fb807d2a907bce50c5e
Adewumi Emmanuel Femi <adewumexemma1@gmail.com>
2020-08-27 19:25:02 +0100

    revert "feat-usersignup feature"
    This reverts commit a425f644ee60d10a2363c2babdfcc2129eddeb27.

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import header from './container/Header';
 import './App.css';
 import Error404 from './container/Error404';
-import Login from './pages/Login';
-import styled, { createGlobalStyle } from 'styled-components';
-import Signup from './pages/Signup';
 
 const App = () => {
   return (
     <Router>
-      <GlobalStyle />
       <Switch>
         <Route exact path='/' component={header} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup' component={Signup} />
         <Route path='*' component={Error404} />
       </Switch>
     </Router>
   );
 };
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    font-family: 'Playfair Display', Arial, Helvetica, sans-serif;
-  }
-
-  body {
-    font-family: 'Playfair Display', Arial, Helvetica, sans-serif;
-  }
-`;
 
 export default App;
